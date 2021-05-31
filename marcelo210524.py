2501f012b8088d556776a9da392b8e6a5995ee68
MarceloRicardoLopesJunior <84341271+MarceloRicardoLopesJunior@users.noreply.github.com>
2021-05-31 13:10:11 -0300

    Add files via upload

---
+++ marcelo210524.py
@@ -0,0 +1,16 @@
+name = "Marcelo"
+age = 16
+print(name + " " + str(age)+ "!")
+first_name= "marcelo"
+last_name= "lopes"
+full_name= first_name +" " + last_name
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
+print(first_name +"\n\t" + last_name)
+favorite_food="  hamburguer  "
+print(favorite_food +"!")
+print(favorite_food.strip() +"!")
+print("\novo")
+print(102 / 23)
+sao paulo campeao paulista 2021!
